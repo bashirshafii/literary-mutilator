9a970399b89d9d97e328666c4b0d70ff54445311
Bashir Shafii <bashir.shafii@gmail.com>
2018-08-11 17:24:19 -0500

    created a function in the js file

---
+++ literary-mutilator.js
@@ -0,0 +1,5 @@
+function findAndReplace() {
+	var oldword = document.getElementById("firstparagraph").innerHTML;
+	var  newword = str.replace(Fluctuations, "Changes");
+	document.getElementById("firstparagraph").innerHTML = newword;
+}
