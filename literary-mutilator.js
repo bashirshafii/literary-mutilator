1c7095c59fdc76495db12025129f59369becb678
Bashir Shafii <bashir.shafii@gmail.com>
2018-08-14 14:28:00 -0500

    added a js function to highlight words in my html document onlick

--- literary-mutilator.js
+++ literary-mutilator.js
@@ -1,9 +1,24 @@
 
+//replace words function
+
 function replaceWords() {
 	var oldText = document.getElementById("01").innerHTML;
 	var newText = oldText.replace(/frequency/gi, "incidence");
 	document.getElementById("01").innerHTML = newText;
 }
+
+//highlight words function
+function highlight(text) {
+	var inputText = document.getElementById("inputText");
+	var innerHTML = inputText.innerHTML;
+	var index = innerHTML.indexOf(text);
+	if (index >= 0) {
+		innerHTML = innerHTML.substring(0,index) + "<span class='highlight'>" + innerHTML.substring(index,index+text.length) + "</span>" + innerHTML.substring(index + text.length);
+		inputText.innerHTML = innerHTML;
+	}
+}
+
+
 
 
 
